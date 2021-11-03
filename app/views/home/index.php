7e637167043407ef23c94eb0c38efa79d1a5f891
gdodd <desarrollowebdodd@gmail.com>
2021-11-03 14:20:15 -0500

    new commit

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,7 +5,7 @@ require_once PATH_TO . 'resource/ClassLoader.php';
 
 use resource\{ClassLoader,ComponentView};
 use layout\AppTemplate;
-
 new ClassLoader;
+
 AppTemplate::app('Inicio - Dashboard', ComponentView::render('home'));
 AppTemplate::header_location();
