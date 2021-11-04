c13fb43395365cc7578cb2a3377c1ddf74e91b25
gdodd <desarrollowebdodd@gmail.com>
2021-11-04 15:40:23 -0500

    new commit

--- resource/SetToken.php
+++ resource/SetToken.php
@@ -6,6 +6,6 @@ class SetToken
 {
     public static function token()
     {
-        return substr(str_shuffle('1234567890abcdefghijklmnopqrstuxyzABCDEFGHIJKLMNOPQRSTUXYZ'),0,12).\resource\Encrypt::datum('encrypt','tlt'.(time()+24*60*60));
+        return substr(str_shuffle('1234567890abcdefghijklmnopqrstuxyzABCDEFGHIJKLMNOPQRSTUXYZ'),0,12).\resource\Encrypt::datum('encrypt','III'.(time()+24*60*60));
     }
 }
