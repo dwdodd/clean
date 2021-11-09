448bb49a0347cb6148a364fb65995ed3854cdb2c
gdodd <desarrollowebdodd@gmail.com>
2021-11-09 15:53:55 -0500

    new commit

--- app/views/home/component/home.php
+++ app/views/home/component/home.php
@@ -6,27 +6,29 @@
 
 <div id="list"></div>
 
-<script type="module">
-    import host from "{{host}}app/assets/js/base_url.js";
-    const getList = async () => {
-        const list = document.getElementById('list');
-        const frag = document.createDocumentFragment();
-        const nombre = '';
-        const token  = document.getElementById('token').value;
-        const get = await fetch(`api/v1/services/user/user-list.php`,{
-            method:'post',
-            body: JSON.stringify({ nombre, token }),
-            headers:{ 'Content-Type': 'application/json' }
-        });
-        if( get.ok ){
-            let info = await get.json();
-            info.items.forEach(element => {
-                let p = document.createElement('p');
-                p.innerText = `Usuario: ${element.usuario}\nCorreo: ${element.correo}`;
-                frag.appendChild(p);
-            });list.appendChild(frag);
-        }else{
-            alert( get.status )
+<script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const getList = async () => {
+            const list = document.getElementById('list');
+            const frag = document.createDocumentFragment();
+            const nombre = '';
+            const token  = document.getElementById('token').value;
+            const get = await fetch(`api/v1/services/user/user-list.php`,{
+                method:'post',
+                body: JSON.stringify({ nombre, token }),
+                headers:{ 'Content-Type': 'application/json' }
+            });
+            if( get.ok ){
+                let info = await get.json();
+                info.items.forEach(element => {
+                    let p = document.createElement('p');
+                    p.innerText = `Usuario: ${element.usuario}\nCorreo: ${element.correo}`;
+                    frag.appendChild(p);
+                });list.appendChild(frag);
+            }else{
+                alert( get.status )
+            }
         }
-    };getList();
+        getList();
+    });
 </script>
