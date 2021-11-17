8a4eb1e23a29878ed70cead512f2a428631cb29f
gdodd <desarrollowebdodd@gmail.com>
2021-11-17 00:45:56 -0500

    new commit

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -3,18 +3,14 @@
 define('PATH_TO', substr(dirname(__FILE__),0,22));
 require_once PATH_TO . 'autoload.php';
 
-use app\views\home\helpers\HomeHook;
-use app\layout\Template;
-
 resource\UrlParamsCompare::param(['/','home'],$_GET['info']);
 
-Template::app(
-    'Inicio - Dashboard',
-    str_replace(
-        HomeHook::key(),
-        HomeHook::value(),
-        file_get_contents('content/home.php')
-    )
+$content = str_replace(
+    '{{token}}',
+    $_SESSION['token-access'],
+    file_get_contents('content/home.php')
 );
 
-Template::header_location();
+app\layout\Template::app('Inicio - Dashboard', $content);
+
+app\layout\Template::header_location();
