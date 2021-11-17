f0a6cc20063418bf651523e58ab0a07089f22878
gdodd <desarrollowebdodd@gmail.com>
2021-11-17 07:06:08 -0500

    new commit

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,16 +1,26 @@
 <?php
 
-define('PATH_TO', substr(dirname(__FILE__),0,22));
+require_once dirname(__DIR__).'/path.php';
 require_once PATH_TO . 'autoload.php';
 
-resource\UrlParamsCompare::param(['/','home'],$_GET['info']);
+$param = trim($_GET['info'],'/');
+resource\UrlParamsCompare::param(['/','value'], $param);
 
-$content = str_replace(
-    '{{token}}',
-    $_SESSION['token-access'],
-    file_get_contents('content/home.php')
-);
+switch($param) {
+    case 'value':
+        exit("Dale valor...");
+    break;
+    
+    default:
+        $title = 'Inicio - Dashboard';
+        $content = str_replace(
+            '{{token}}',
+            $_SESSION['token-access'],
+            file_get_contents('content/home.php')
+        );
+    break;
+}
 
-app\layout\Template::app('Inicio - Dashboard', $content);
+app\layout\Template::app($title, $content);
 
 app\layout\Template::header_location();
--- resource/UrlParamsCompare.php
+++ resource/UrlParamsCompare.php
@@ -6,11 +6,12 @@ class UrlParamsCompare
 {
     public static function param($verbs=[], $param)
     {
+        $param = trim($param,'/');
         if( $param && !in_array($param, $verbs) ) exit(self::text($param));
     }
 
     private static function text($param)
     {
-        return '<div style="color:#0A43C8;text-align:center;margin-top:5%;font-size:80px;font-weight:bold;font-family:tahoma;">404<br>Not Found ('.ltrim($param,'/').')</div>';
+        return '<div style="color:#0A43C8;text-align:center;margin-top:5%;font-size:80px;font-weight:bold;font-family:tahoma;">404<br>Not Found ('.$param.')</div>';
     }
 }
