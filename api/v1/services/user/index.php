7d8bbc5c4418aba87e09ffa2587cb5b83c3a8613
gdodd <desarrollowebdodd@gmail.com>
2021-11-23 10:19:59 -0500

    new commit

--- api/v1/services/user/index.php
+++ api/v1/services/user/index.php
@@ -5,14 +5,9 @@ require_once PATH_TO.'autoload.php';
 
 $_POST = json_decode(file_get_contents("php://input"), true);
 
-switch (ltrim($_GET['info'],'/')) {
-    case 'list':
-        new src\user\services\GetUserListService( (object)$_POST );
-    break;
-    case 'create':
-        new src\user\services\CreateUserService( (object)$_POST );
-    break;
-    default:
-        exit('Error ID: <b>'.rand().'</b>');
-    break;
+switch(ltrim($_GET['info'],'/'))
+{
+    case 'list':   new src\user\services\GetUserListService( (object)$_POST ); break;
+    case 'create': new src\user\services\CreateUserService( (object)$_POST ); break;
+    default: exit('Error ID: <b>'.rand().'</b>'); break;
 }
